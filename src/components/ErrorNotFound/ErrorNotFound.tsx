9c6abfd1187694d74b51662f14b5734ecacfd286
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-03-13 21:26:16 +0300

    styles and structure for Header

--- src/components/ErrorNotFound/ErrorNotFound.tsx
+++ src/components/ErrorNotFound/ErrorNotFound.tsx
@@ -1,10 +1,13 @@
 import alert_logo from './img/alert.png';
+import styles from './ErrorNotFound.module.css';
 
 const ErrorNotFound = () => {
   return (
-    <section>
-      <img src={alert_logo} alt="alert" />
-      <div>Ошибка: не удалось загрузить информацию</div>
+    <section className={styles.alert_container}>
+      <img src={alert_logo} className={styles.alert_icon} alt="alert" />
+      <div className={styles.alert_message}>
+        Ошибка: не удалось загрузить информацию
+      </div>
     </section>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import ErrorNotFound from '../ErrorNotFound/ErrorNotFound';
 import logo from './img/MatchTracker.png';
+import refresh_icon from './img/refresh_icon.png';
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <img src={logo} alt="logo" />
+    <header className={styles.header}>
+      <img src={logo} alt="logo" className={styles.logo} />
+      <div className={styles.refresh_data_panel}>
+        <ErrorNotFound />
+        <button className={styles.refresh_btn}>
+          Обновить
+          <img src={refresh_icon} alt="refresh_icon" />
+        </button>
       </div>
-      <ErrorNotFound />
     </header>
   );
 };
