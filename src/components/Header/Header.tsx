8e51db70dd2c44a19a8607f5ab974d72ae63a433
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-03-14 17:21:30 +0300

    add logic for Error in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,16 @@ import ErrorNotFound from '../ErrorNotFound/ErrorNotFound';
 import logo from './img/MatchTracker.png';
 import refresh_icon from './img/refresh_icon.png';
 import styles from './Header.module.css';
+import { useError } from '../../context/ErrorContext'
 
 const Header = () => {
+  const { error } = useError();
+
   return (
     <header className={styles.header}>
       <img src={logo} alt="logo" className={styles.logo} />
       <div className={styles.refresh_data_panel}>
-        <ErrorNotFound />
+        {error && <ErrorNotFound />}
         <button className={styles.refresh_btn}>
           Обновить
           <img
